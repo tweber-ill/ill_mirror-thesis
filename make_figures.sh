5dedcaa80022ce6ee4d7c4c766497e1d84b72d80
Tobias Weber <tobias.weber@tum.de>
2021-07-03 11:34:57 +0200

    a) continued with tas model; b) created axis figures

--- make_figures.sh
+++ make_figures.sh
@@ -6,6 +6,16 @@
 # @license GPLv3 (see 'LICENSE' file)
 #
 
+
+CONVERTER=$(which inkscape)
+
+
+if [ "$CONVERTER" = "" ]; then
+	echo -e "\n\nError: Inkscape, which is used for conversion, was not found.\n\n"
+	exit -1
+fi
+
+
 for fig_svg in figures_svg/*.svg; do
 	# change ending
 	fig_pdf_meta="${fig_svg%\.svg}.pdf"
@@ -23,6 +33,6 @@ for fig_svg in figures_svg/*.svg; do
 
 	echo -e "$fig_svg -> $fig_pdf"
 
-	convert $fig_svg $fig_pdf
+	#convert -render $fig_svg $fig_pdf
+	$CONVERTER --export-pdf-version=1.4 --export-pdf=$fig_pdf $fig_svg
 done
-
